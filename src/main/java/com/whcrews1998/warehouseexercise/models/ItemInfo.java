701a5d19a9eb7a39a9be39bb2323bc8e180ae4b0
Whcrews1998 <whcrews1998@gmail.com>
2023-04-08 11:23:41 -0500

    Created an ItemInfo module for caching and manipulating information before persisting to OrderDetails Entity.

---
+++ src/main/java/com/whcrews1998/warehouseexercise/models/ItemInfo.java
@@ -0,0 +1,25 @@
+package com.whcrews1998.warehouseexercise.models;
+
+public class ItemInfo {
+    private int id;
+    private String name;
+
+    public ItemInfo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
