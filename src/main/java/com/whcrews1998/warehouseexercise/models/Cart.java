b2499315a8dd68feae7ec4478e9a6b3e79554d7b
Whcrews1998 <whcrews1998@gmail.com>
2023-04-08 11:18:38 -0500

    Made the Cart Module cacheable so I can store the data within the User's session, instead of persiting it to DB.

--- src/main/java/com/whcrews1998/warehouseexercise/models/Cart.java
+++ src/main/java/com/whcrews1998/warehouseexercise/models/Cart.java
@@ -1,14 +1,28 @@
 package com.whcrews1998.warehouseexercise.models;
 
 import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
 
-@Entity
+import java.util.ArrayList;
+import java.util.List;
+
+@Cacheable
 public class Cart {
-    @Id
-    @GeneratedValue
+
     private Integer id;
+    private List<ItemInfo> itemList;
 
     public Cart() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public Cart(Integer id, List<ItemInfo> itemList) {
+        this.id = id;
+        this.itemList = itemList;
+    }
+
+    public void addToCart(ItemInfo item) {
+        itemList.add(item);
     }
 
     public Integer getId() {
@@ -19,4 +33,11 @@ public class Cart {
         this.id = id;
     }
 
+    public List<ItemInfo> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<ItemInfo> itemList) {
+        this.itemList = itemList;
+    }
 }
